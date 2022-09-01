13552d9a336cf328ba97981953fdaab355a61548
jendos <huiartman@gmail.com>
2022-09-01 13:47:12 +0300

    4 commit TicTacToe.py
    method human_go

--- TicTacToe.py
+++ TicTacToe.py
@@ -7,7 +7,7 @@ class TicTacToe:
     COMPUTER_O = 2  # нолик (игрок - компьютер)
 
     def __init__(self):
-        self._pole = tuple(tuple(Cell() for _ in range(3)) for _ in range(3))
+        self.pole = tuple(tuple(Cell() for _ in range(3)) for _ in range(3))
 
     @staticmethod
     def __check_index(indx):
@@ -16,21 +16,35 @@ class TicTacToe:
 
     def __getitem__(self, item):
         self.__check_index(item)
-        return self._pole[item[0]][item[1]].value
+        return self.pole[item[0]][item[1]].value
 
     def __setitem__(self, key, value):
         self.__check_index(key)
-        self._pole[key[0]][key[1]].value = value
+        self.pole[key[0]][key[1]].value = value
+        self.pole[key[0]][key[1]].is_free = False
 
     def init(self):
-        for row in self._pole:
+        for row in self.pole:
             for item in row:
                 item.value = 0
                 item.is_free = True
 
     def show(self):
-        for i, row in enumerate(self._pole):
+        for i, row in enumerate(self.pole):
             for j, item in enumerate(row):
                 print(item.value, end='')
                 print(' | ' if j != 2 else '\n', end='')
             print('─────────' if i != 2 else '')
+
+    def human_go(self):
+        while True:
+            coords = tuple(map(int, input('Введите координаты для Х через пробел: ').split()))
+            cell = self.pole[coords[0]][coords[1]]
+            if cell:
+                cell.value = 1
+                cell.is_free = False
+                break
+            else:
+                print('Клетка занята')
+
+
