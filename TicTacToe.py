2cd9c8dda9c1d89df50f4da82ad121aa0725c88c
jendos <huiartman@gmail.com>
2022-09-01 12:51:49 +0300

    3 commit TicTacToe.py
    init, show

--- TicTacToe.py
+++ TicTacToe.py
@@ -2,6 +2,10 @@ from class_Cell import Cell
 
 
 class TicTacToe:
+    FREE_CELL = 0  # свободная клетка
+    HUMAN_X = 1  # крестик (игрок - человек)
+    COMPUTER_O = 2  # нолик (игрок - компьютер)
+
     def __init__(self):
         self._pole = tuple(tuple(Cell() for _ in range(3)) for _ in range(3))
 
@@ -17,3 +21,16 @@ class TicTacToe:
     def __setitem__(self, key, value):
         self.__check_index(key)
         self._pole[key[0]][key[1]].value = value
+
+    def init(self):
+        for row in self._pole:
+            for item in row:
+                item.value = 0
+                item.is_free = True
+
+    def show(self):
+        for i, row in enumerate(self._pole):
+            for j, item in enumerate(row):
+                print(item.value, end='')
+                print(' | ' if j != 2 else '\n', end='')
+            print('─────────' if i != 2 else '')
